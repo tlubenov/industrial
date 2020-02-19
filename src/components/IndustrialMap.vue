b3139e12324836b2ce2a1397eb9c9b9031bcb461
Todor Lubenov <tlubenov@gmail.com>
2020-02-19 10:51:14 +0200

    clear label on the map

--- src/components/IndustrialMap.vue
+++ src/components/IndustrialMap.vue
@@ -107,20 +107,40 @@
         if (!this.enableTooltip) {
           return () => {};
         }
+
+
         if (this.zoom < 18) {
           return (feature, layer) => {
+            let tt = '';
+            if (feature.properties.puid) {
+              tt += `<div class="small-label">ПИ ${feature.properties.puid}</div>`;
+            }
+            if (feature.properties.area) {
+              tt += `<div class="small-label">ПЛЩ ${feature.properties.area} кв.м.</div>`;
+            }
+            if (feature.properties.description) {
+              tt += `<div class="small-label">БЕЛ. ${feature.properties.description}</div>`;
+            }
             layer.bindTooltip(
-              `<div class="small-label">ПИ ${feature.properties.puid}</div>
-               <div class="small-label">ПЛЩ ${feature.properties.area} кв.м.</div>
-               <div class="small-label">БЕЛ. ${feature.properties.description}</div>
-                `,
+              tt,
               { permanent: false, sticky: false, direction: 'auto' },
             );
           };
         }
         return (feature, layer) => {
+          let tt = '';
+          if (feature.properties.puid) {
+            tt += `<div class="small-label">ПИ ${feature.properties.puid}</div>`;
+          }
+          if (feature.properties.area) {
+            tt += `<div class="small-label">ПЛЩ ${feature.properties.area} кв.м.</div>`;
+          }
+          if (feature.properties.description) {
+            tt += `<div class="small-label">БЕЛ. ${feature.properties.description}</div>`;
+          }
+
           layer.bindTooltip(
-            `<div class="small-label">${feature.properties.puid}</div>`,
+            tt,
             { permanent: true, sticky: false, direction: 'top' },
           );
         };
